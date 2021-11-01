230f88186db87cb4e270c18e31d8ab070c35a082
Nathaniel Lipkins-Warren <nathaniellipkins@gmail.com>
2021-10-31 19:34:37 -0700

    Added torches to the hallways

--- Quake Files/Downloads/quake-leveldesign-starterkit-master/quake-leveldesign-starterkit-master/tools/ericw-tools/compile.bat
+++ Quake Files/Downloads/quake-leveldesign-starterkit-master/quake-leveldesign-starterkit-master/tools/ericw-tools/compile.bat
@@ -20,3 +20,6 @@ C:\tools\vis.exe Akull
 copy Akull.bsp C:\Users\natha\OneDrive\Documents\GitHub\Akull
 copy Akull.pts C:\Users\natha\OneDrive\Documents\GitHub\Akull
 copy Akull.lit C:\Users\natha\OneDrive\Documents\GitHub\Akull
+cd\
+cd C:
+Quake  +map Akull
